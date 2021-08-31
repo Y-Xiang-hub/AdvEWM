5fe60754c097af0b6f17b820447fd0f87d85d602
LemonX <57489506+Y-Xiang-hub@users.noreply.github.com>
2021-08-31 19:22:41 +0800

    Update WaterMark.m

--- old version/single_image_DWT/WaterMark.m
+++ old version/single_image_DWT/WaterMark.m
@@ -1,4 +1,9 @@
-% input
+% @Time     : 1 June, 2020
+% @Author   : Tiantian Li and Yuexin Xiang
+% @Email    : ltt@cug.edu.cn or yuexin.xiang@cug.edu.cn
+% @Function : input
+
+
 function [ImgName] = WaterMark(OriImg,WmImg,AlphaR,AlphaG,AlphaB)
 
 % RGB separation of the original image
