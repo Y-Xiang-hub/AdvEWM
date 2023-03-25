eaa86462538a43707ee5f931001094ea96300d97
Tiantian <tiantian3@student.unimelb.edu.au>
2023-03-25 13:39:51 +0000

    new file:   DWT-DCT/DCT_WaterMark.m new file:   DWT-DCT/DWT_WaterMark.m new file:   DWT-DCT/DWT_nDWT.m new file:   DWT-DCT/combining-DWT-DCT.py

---
+++ DWT-DCT/DWT_nDWT.m
@@ -0,0 +1,36 @@
+function [cA1,cH1,cV1,cD1,cA2,cH2,cV2,cD2,cA3,cH3,cV3,cD3] = DWT_nDWT(I,N)
+if N==3
+    [c,s] = wavedec2(double(I),3,'haar');
+   % [c,s] = wavedec2(I,3,'haar');
+    cA1 = appcoef2(c,s,'haar',1);
+    cH1 = detcoef2('h',c,s,1);
+    cV1 = detcoef2('v',c,s,1);
+    cD1 = detcoef2('d',c,s,1);
+    cA2 = appcoef2(c,s,'haar',2);
+    cH2 = detcoef2('h',c,s,2);
+    cV2 = detcoef2('v',c,s,2);
+    cD2 = detcoef2('d',c,s,2);
+    cA3 = appcoef2(c,s,'haar',3);
+    cH3 = detcoef2('h',c,s,3);
+    cV3 = detcoef2('v',c,s,3);
+    cD3 = detcoef2('d',c,s,3);
+    
+    k = s(2,1)*2 - s(3,1);
+    cH2 = padarray(cH2,[k k],1,'post');
+    cV2 = padarray(cV2,[k k],1,'post');
+    cD2 = padarray(cD2,[k k],1,'post');
+    
+    k = s(2,1)*4 - s(4,1);
+    cH1 = padarray(cH1,[k k],1,'post');
+    cV1 = padarray(cV1,[k k],1,'post');
+    cD1 = padarray(cD1,[k k],1,'post');
+end
+
+if N==1
+    [c,s] = wavedec2(double(I),1,'haar');
+   % [c,s] = wavedec2(I,1,'haar');
+    cA1 = appcoef2(c,s,'haar',1);
+    cH1 = detcoef2('h',c,s,1);
+    cV1 = detcoef2('v',c,s,1);
+    cD1 = detcoef2('d',c,s,1);
+end
